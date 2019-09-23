eb1e70492043a09b3040d2b5a96694223e953cfa
K. Adam White <kadamwhite@users.noreply.github.com>
2019-09-23 13:09:21 -0400

    44975.6.diff

--- src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-posts-controller.php
@@ -1039,8 +1039,8 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 		// Sending a null date or date_gmt value resets date and date_gmt to their
 		// default values (`0000-00-00 00:00:00`).
 		if (
-			( $request->has_param( 'date_gmt' ) && null === $request['date_gmt'] ) ||
-			( $request->has_param( 'date' ) && null === $request['date'] )
+			( ! empty( $schema['properties']['date_gmt'] ) && $request->has_param( 'date_gmt' ) && null === $request['date_gmt'] ) ||
+			( ! empty( $schema['properties']['date'] ) && $request->has_param( 'date' ) && null === $request['date'] )
 		) {
 			$prepared_post->post_date_gmt = null;
 			$prepared_post->post_date     = null;
