a8d2444ec62718895f8d961bc2416cdd8deb3494
K. Adam White <kadamwhite@users.noreply.github.com>
2019-10-09 17:36:27 -0400

    Simplify test case for sibling inclusion

--- tests/phpunit/tests/rest-api.php
+++ tests/phpunit/tests/rest-api.php
@@ -570,37 +570,22 @@ class Tests_REST_API extends WP_UnitTestCase {
 
 		$response->set_data(
 			array(
-				'a' => 0,
-				'b' => array(
-					'1' => 1,
-					'2' => array (
-						'aa' => 1,
-						'bb' => 2,
-					),
-				),
-				'c' => 3,
-				'd' => array(
-					'4' => 4,
-					'5' => 5,
+				'meta' => array(
+					'key1' => 1,
+					'key2' => 2,
 				),
 			)
 		);
 		$request = array(
-			'_fields' => 'b.2.bb,b.2.aa,d.4,d.5',
+			'_fields' => 'meta.key1,meta.key2',
 		);
 
 		$response = rest_filter_response_fields( $response, null, $request );
 		$this->assertEquals(
 			array(
-				'b' => array(
-					'2' => array (
-						'aa' => 1,
-						'bb' => 2,
-					),
-				),
-				'd' => array(
-					'4' => 4,
-					'5' => 5,
+				'meta' => array(
+					'key1' => 1,
+					'key2' => 2,
 				),
 			),
 			$response->get_data()
